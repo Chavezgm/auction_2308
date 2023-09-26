14b6d4db6fae29344223fb38fa2974d3f46c8a52
Martin Chavez <martinchavez1203@gmail.com>
2023-09-26 09:30:57 -0600

    Item exists

---
+++ spec/item_spec.rb
@@ -0,0 +1,18 @@
+require './lib/item'
+
+RSpec.describe Item do 
+  before (:each) do 
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
+  end
+  
+  describe 'Instanciates' do 
+    it 'It exisit' do 
+      expect(@item1).to be_a(Item)
+      expect(@item2).to be_a(Item)
+      
+      expect(@item1.name).to eq("Chalkware Piggy Bank")
+      expect(@item2.name).to eq("Bamboo Picture Frame")
+    end
+  end
+end
